9f605fccc3b58d8f71d863a4015f4a8a081929a2
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-04-28 14:07:37 +0200

    :hammer: Add display feature for keyPress

--- src/components/DrumMachine.jsx
+++ src/components/DrumMachine.jsx
@@ -15,25 +15,41 @@ import KickNHat from '../static/audio/kick-n-hat.mp3'
 import Kick from '../static/audio/kick.mp3'
 import OpenHH from '../static/audio/open-hh.mp3'
 
+import { useState } from 'react';
+
 function DrumMachine() {
     const drumPads = [
-        { key: 1, char: "Q", color: "#FF5F7A", src: Clap },
-        { key: 2, char: "W", color: "#ECEAEC", src: ClosedHH },
-        { key: 3, char: "E", color: "#4BC3EF", src: Heater1 },
-        { key: 4, char: "A", color: "#4BC3EF", src: Heater2 },
-        { key: 5, char: "S", color: "#FF5F7A", src: Heater3 },
-        { key: 6, char: "D", color: "#ECEAEC", src: Heater4 },
-        { key: 7, char: "Z", color: "#ECEAEC", src: KickNHat },
-        { key: 8, char: "X", color: "#4BC3EF", src: Kick },
-        { key: 9, char: "C", color: "#FF5F7A", src: OpenHH },
+        { key: 1, char: "Q", color: "#FF5F7A", src: Clap, srcName: 'Clap' },
+        { key: 2, char: "W", color: "#ECEAEC", src: ClosedHH, srcName: 'Closed HH' },
+        { key: 3, char: "E", color: "#4BC3EF", src: Heater1, srcName: 'Heater 1' },
+        { key: 4, char: "A", color: "#4BC3EF", src: Heater2, srcName: 'Heater 2' },
+        { key: 5, char: "S", color: "#FF5F7A", src: Heater3, srcName: 'Heater 3' },
+        { key: 6, char: "D", color: "#ECEAEC", src: Heater4, srcName: 'Heater 4' },
+        { key: 7, char: "Z", color: "#ECEAEC", src: KickNHat, srcName: 'Kick n\'Hat' },
+        { key: 8, char: "X", color: "#4BC3EF", src: Kick, srcName: 'Kick' },
+        { key: 9, char: "C", color: "#FF5F7A", src: OpenHH, srcName: 'Open HH' },
     ];
     let controllers = [];
+    const [displayText, setDisplayText] = useState('');
 
-    let handleKeyDown = (event) => document.getElementById(event.key.toUpperCase()).click();
+    function handleKeyDown(event) {
+        try {
+            let drumPad = document.getElementById(event.key.toUpperCase());
+            drumPad.click();
+            setDisplayText(drumPad.getAttribute('name'));
+            setTimeout(() => setDisplayText(''), 1000);
+        } catch (error) {
+            // Drumpad key doesn't exist
+        }
+    }
+
+    function handleOnClick(event){
+        console.log(event);
+    }
 
     for (let i = 0; i < 4; i++)
         controllers.push(<Controller
-            style={{ rotate: `${Math.floor(Math.random() * (361))}deg` }}
+            style={{ rotate: `${40 ** i}deg` }}
             key={i + 1}
         />);
 
@@ -53,13 +69,14 @@ function DrumMachine() {
                                 value={item.char}
                                 bgColor={item.color}
                                 src={item.src}
+                                name={item.srcName}
                             />
                         })}
                     </div>
                     <OnOffSwitch />
                     <OnOffSwitch />
                 </div>
-                <div id="display" />
+                <div id="display">{displayText}</div>
             </div>
         </div>
     );
--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,9 +1,9 @@
-function DrumPad({ value, bgColor, src }) {
+function DrumPad({ value, bgColor, src, name }) {
     let handleSound = () => new Audio(src).play();
     let style = { backgroundColor: bgColor };
 
     return (
-        <div id={value} className="drum-pad btn" style={style} onClick={handleSound}>
+        <div id={value} className="drum-pad btn" style={style} onClick={handleSound} name={name}>
             {value}
         </div>
     );
